d142b9499a0e59cc6d82ea09d3dd089a7787f662
Fracanta98 <thefracanta98@hotmail.com>
2025-12-10 17:38:11 +0100

    aggiunta classe GS_SETTINGS

---
+++ bin2bin/src/app/Models/gs-settings.spec.ts
@@ -0,0 +1,7 @@
+import { GSSETTINGS } from './gs-settings';
+
+describe('GSSETTINGS', () => {
+  it('should create an instance', () => {
+    expect(new GSSETTINGS()).toBeTruthy();
+  });
+});
---
+++ bin2bin/src/app/Models/gs-settings.ts
@@ -0,0 +1,11 @@
+export class GSSETTINGS {
+     LGNUM?: string;
+    LNUMT?: string;
+    WERKS?: string;
+    LGORT?: string;
+    PADEST?: string;
+    TPLMA?: string;
+    SHORT_TEXT?: string;
+    DESCRIPTION?: string;
+    APP?: string;
+}
