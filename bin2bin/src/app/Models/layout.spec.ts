9a660c6aefe84a8cc4f8c5a424c57ab1b01f88d0
Fracanta98 <thefracanta98@hotmail.com>
2025-12-10 17:43:11 +0100

    aggiunta classe LAYOUT

---
+++ bin2bin/src/app/Models/layout.spec.ts
@@ -0,0 +1,7 @@
+import { LAYOUT } from './layout';
+
+describe('LAYOUT', () => {
+  it('should create an instance', () => {
+    expect(new LAYOUT()).toBeTruthy();
+  });
+});
---
+++ bin2bin/src/app/Models/layout.ts
@@ -0,0 +1,12 @@
+export class LAYOUT {
+      WERKS?: string;
+      LGNUM?: string;
+      APPS_GRP?: string;
+      APPS_ID?: string;
+      SCREEN_NUM?: string;
+      FIELDNAME?: string;
+      COL_POSITION?: number;
+      VISIBLE?: string;
+      SORT_ORDER?: string;
+      SORT_TYPE?: string;
+}
